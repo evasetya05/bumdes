44e24c2c20c6c9efbd428762c7775493f91fd436
Eva Setyabudi <eva.setyabudi1986@gmail.com>
2025-12-17 16:26:22 +0700

    sudah bisa ledger perlu tambahan menu untuk parkir dari excel

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import home, profile, RegisterView
 
+app_name = 'users'
+
 urlpatterns = [
     path('', home, name='users-home'),
     path('register/', RegisterView.as_view(), name='users-register'),
