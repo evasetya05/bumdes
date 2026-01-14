3fcf1a6dc8a73f0c5c7eb7b750711263a7fade96
Eva Setyabudi <eva.setyabudi1986.com>
2026-01-14 14:32:45 +0700

    sudah bisa untuk report

--- apps/modules/ledger/templatetags/ledger_extras.py
+++ apps/modules/ledger/templatetags/ledger_extras.py
@@ -1,4 +1,5 @@
 from django import template
+
 from apps.modules.ledger.models import ClosingPeriod
 
 register = template.Library()
--- apps/modules/ledger/urls/index.py
+++ apps/modules/ledger/urls/index.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from apps.modules.ledger.views.index import index
 from apps.modules.ledger.views.ledger_report import ledger_report
 
