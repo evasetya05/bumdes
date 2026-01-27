0b499905c6ca3c47b67c11a381a3dafc21afd2c7
Eva Setyabudi <eva.setyabudi1986@gmail.com>
2026-01-27 18:46:31 +0700

    passanger5

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -4,7 +4,7 @@ import sys
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, BASE_DIR)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bumdes.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bumdes.settings.production")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
